32b1bb426e6dbf546baf992d3eeaf4c9cbd5eea7
Rustem Islamuratov <rustem.islamuratov@gmail.com>
2020-03-22 20:38:28 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+
+const appName = `http://localhost:4000`;
+const appURL = `http://localhost:4000`;
+
+function createWindow() {
+  const browserWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      webviewTag: true,
+    },
+  });
+  browserWindow.setTitle(appName);
+  browserWindow.webContents.openDevTools();
+  // win.loadURL(`file://${__dirname}/index.html`);
+  browserWindow.loadURL(appURL);
+}
+
+app.on('ready', createWindow);
